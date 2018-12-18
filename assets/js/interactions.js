c5cdb8a513062b1dd1da91472a067e066d39de22
dereckachy <frederickagubuzu@gmail.com>
2018-12-18 08:12:40 +0100

    js fix

--- assets/js/interactions.js
+++ assets/js/interactions.js
@@ -32,7 +32,7 @@ function toggleMenu() {
 
 }
 
-if (document.location.href==="http://localhost:7880/html/pages/") {
+if (document.location.href==="https://dereckachy.github.io/dereckachystudios") {
   var heroTextSvg = document.querySelector('#hero-text-svg');
 
   if (matchMedia) {
@@ -46,7 +46,7 @@ if (document.location.href==="http://localhost:7880/html/pages/") {
 
 }
 
-if (document.location.href==="http://localhost:7880/html/pages/contact/") {
+if (document.location.href==="https://dereckachy.github.io/dereckachystudios/contact/") {
 
   var inputGroup = document.getElementsByClassName('input-group');
   var textBox = document.querySelector('#text-area');
@@ -56,31 +56,7 @@ if (document.location.href==="http://localhost:7880/html/pages/contact/") {
     inputGroup[i].addEventListener('click', function (event) {
       this.firstElementChild.focus();
       this.children[2].firstElementChild.style.transformOrigin = ''+ event.offsetX +'px center 0px'
-            // body.classList.add('no-scroll');
     })
-
-    // inputGroup[i].firstElementChild.addEventListener('focus', function () {
-    //   console.log(i)
-    //   if (this.classList.contains('in-focus')) {}
-    //   else { this.parentElement.classList.add('in-focus')
-    //          this.parentElement.classList.remove('invisible')
-    //          this.parentElement.parentElement.classList.add('in-focus')
-    //          this.parentElement.previousElementSibling.classList.remove('in-focus')
-    //          this.parentElement.previousElementSibling.classList.add('invisible')
-    //          var $this = this;
-    //          setTimeout(function () {
-    //            $this.parentElement.previousElementSibling.classList.remove('invisible')
-    //          }, 1200)
-    //          body.classList.add('no-scroll')}
-    // })
-    // inputGroup[i].firstElementChild.addEventListener('blur', function () {
-    //   this.parentElement.parentElement.classList.remove('in-focus')
-    //   this.parentElement.classList.add('invisible')
-    //   this.parentElement.classList.remove('in-focus')
-    //   body.classList.toggle('no-scroll')
-    //   for (var i = 0; i < inputGroup.length; i++) {
-    //       inputGroup[i].classList.remove('invisible')}
-    // })
   }
 
   for (var  i = 0; i < inputGroup.length; i++) {
@@ -90,23 +66,5 @@ if (document.location.href==="http://localhost:7880/html/pages/contact/") {
         else { this.classList.remove('has-text'); }
     })
   }
-
-  // textBox.addEventListener('input', function () {
-  //   var textLength = textBox.value.length
-  //   hiddenTextBox.textContent += textBox.value.charAt((textLength - 1))
-  //   var bBox = hiddenTextBox.getBoundingClientRect();
-  //   var extraHeight = (bBox.height - 33)
-  //   if (bBox.height > 33 && bBox.height < 330) {
-  //     textBox.style.height = '' + (22 + extraHeight) + 'px'
-  //   } else if (bBox.height >= 330 ) {
-  //     textBox.style.overflowY = 'scroll'
-  //     textBox.style.paddingBottom = 0
-  //     textBox.style.marginBottom = '17px'
-  //   }
-  //   if (textBox.value === "") {
-  //     hiddenTextBox.textContent = ""
-  //     textBox.style.height = ""
-  //   }
-  // })
 
 }
